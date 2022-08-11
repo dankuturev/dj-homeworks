c091ec7b04ecea20311eafddbc16a029762daf60
Danila <dan.kutyrev@yandex.ru>
2022-08-11 23:38:48 +0300

    Added first task

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,7 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
-
+from django.utils.text import slugify
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -11,7 +11,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
-
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(
+                id=int(phone['id']),
+                name=phone['name'],
+                price=int(phone['price']),
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=slugify(phone['name'])
+            )
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,21 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sorting = request.GET.get('sort')
+    all_phones = Phone.objects.all()
+    if sorting == 'name':
+        all_phones = all_phones.order_by('name')
+    elif sorting == 'min_price':
+        all_phones = all_phones.order_by('price')
+    elif sorting == 'max_price':
+        all_phones = all_phones.order_by('-price')
+
+    context = {'phones': all_phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
